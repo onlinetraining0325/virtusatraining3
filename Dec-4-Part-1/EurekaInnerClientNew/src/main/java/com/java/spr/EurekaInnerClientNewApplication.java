2be1934d301f8d230ad00aec0a98e3fac104e9cf
b69user39@rpsconsulting.in <prasanna.trainer@gmail.com>
2025-12-04 11:31:54 +0530

    code

---
+++ Dec-4-Part-1/EurekaInnerClientNew/src/main/java/com/java/spr/EurekaInnerClientNewApplication.java
@@ -0,0 +1,24 @@
+package com.java.spr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaInnerClientNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaInnerClientNewApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+}
---
+++ Dec-4-Part-1/EurekaInnerClientNew/src/main/java/com/java/spr/TestController.java
@@ -0,0 +1,29 @@
+package com.java.spr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+//	@GetMapping("/inter/{id}")
+//	public Object getEmploy(@PathVariable int id) {
+//		Object employs = restTemplate.getForObject("http://eureka-client-new/"+id, Object.class);
+//		return employs;
+//	}
+//	
+	@GetMapping("/")
+	public String showEmploy() {
+        return restTemplate.getForObject("http://EUREKACLIENT", String.class);
+	}
+    @GetMapping("/test")
+    public String showEmplos() {
+        return restTemplate.getForObject("http://EUREKACLIENT/test", String.class);
+    }
+}
